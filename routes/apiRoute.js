6a5d1893345b44ba01192246714fbc4341c0e56a
Rinzler8806 <shane.rogers916@gmail.com>
2021-03-03 18:25:51 -0800

    updated lines 4-7 on apiroute file

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+  .then(dbNewWorkout => {
+      res.json(dbNewWorkout);
+  })
+  .catch(err => {
+      res.json(err);
+  });
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
